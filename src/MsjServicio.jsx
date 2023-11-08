20fe2f3472af2ecf8dc179c842047088a7938533
JorgeVC26 <jorvillacar26@gmail.com>
2023-11-08 16:40:43 -0600

    jorge

--- src/MsjServicio.jsx
+++ src/MsjServicio.jsx
@@ -6,8 +6,6 @@ function MsjServicio() {
       <div style={{  backgroundColor: '#4CAF50', borderRadius: '2rem' }} className="confirmation-popup mensajeServicio">
         <p style={{ color: '#4CAF50',  }}>Servicio enviado para su aprobación</p>
 
-
-
       </div >
     );
   }
