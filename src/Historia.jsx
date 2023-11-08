8ae995e9ed4fceecd7504c5c73b6d89f153c5279
JorgeVC26 <jorvillacar26@gmail.com>
2023-11-07 21:25:34 -0600

    Revert "diseño pagina historia"
    This reverts commit ec61704be07f8be0e57777caa4f62af05b355a3e.

--- src/Historia.jsx
+++ src/Historia.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import './Style/historia_carrousel.css';
 
-import Footer from "./footer/footer"
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
+import image1 from './img/imagen.png'; 
+import image2 from './img/img.jpg';
+import image3 from './img/img.jpg';
 
 function Home() {
-
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+  
     return (
         <div>
           {/* */}
@@ -13,12 +26,8 @@ function Home() {
             <h1>Safe<span>Pets</span></h1>
           </a>
             </header>
-            <div className="hero">
-            <div className="conocenos">
-              <h1>Conocenos</h1>
-            </div>
-            </div>
-           
+
+      <div className="card">
         <div className="card-content">
           <div className="card-text">
             <h2 className="card-title">Nuestra Historia</h2>
@@ -29,34 +38,21 @@ function Home() {
             tecnología y cuidado animal para garantizar que nuestras queridas mascotas estén bien atendidas.</p>
           </div>
         </div>
+        <div className="card-image">
+          <Slider {...settings}>
+            <div>
+              <img src={image1} alt="Imagen de mascota 1" className="pet-image" />
+            </div>
+            <div>
+              <img src={image2} alt="Imagen de mascota 2" className="pet-image" />
+            </div>
+            <div>
+              <img src={image3} alt="Imagen de mascota 3" className="pet-image" />
+            </div>
+          </Slider>
+        </div>
         
-        <div className="contenedor-imagenes">
-        <div className="imagen-con-texto">
-      <div className="imagen-izquierda"></div>
-      <div className="texto"><h4>Misión: 
-            Nuestra misión es brindar comodidad, seguridad y bienestar 
-            a las mascotas y sus dueños. Nos comprometemos a facilitar 
-            la vida de los dueños de mascotas ocupados al ofrecer servicios de 
-            paseo y cuidado de mascotas confiables y de calidad. Nuestra pasión es 
-            garantizar que cada mascota reciba el amor, atención y ejercicio que merece, 
-            al tiempo que creamos oportunidades significativas para nuestros cuidadores. Nos 
-            esforzamos por construir una comunidad de amantes de mascotas comprometidos 
-            con la felicidad y salud de los animales que hacen parte de nuestras vidas.</h4>
-    </div>
       </div>
-      <div className="imagen-con-texto">
-      <div className="imagen-derecha"></div>
-      <div className="texto"><h4>Visión: 
-            Nuestra visión es convertirnos en la plataforma confiable en 
-            servicios de cuidado y paseo de mascotas, reconocida por su 
-            compromiso con el bienestar de los animales y la confianza de los 
-            dueños de mascotas en las diferentes zonas. Buscamos innovar constantemente, 
-            ampliando nuestros servicios y llegando a nuevas comunidades para brindar 
-            soluciones integrales para las necesidades de las mascotas y sus dueños.</h4>
-    </div>
-      </div>
-    </div>
-    <Footer />
       </div>
     );
   }
