f42a811745a83cf4c0842590f9a2b22cc0eace12
Kerlin-C <kerlin.carranza.solano@est.una.ac.cr>
2023-11-08 16:21:24 -0600

    sadasd

--- src/MsjServicio.jsx
+++ src/MsjServicio.jsx
@@ -5,9 +5,10 @@ function MsjServicio() {
     return (
       <div style={{  backgroundColor: '#4CAF50', borderRadius: '2rem' }} className="confirmation-popup mensajeServicio">
         <p style={{ color: '#4CAF50',  }}>Servicio enviado para su aprobación</p>
-        
-      </div>
+        <p style={{ color: '#4CAF50',  }}>Servicio enviado para su aprobación</p>
+
+      </div >
     );
   }
- 
+     
 export default MsjServicio;
